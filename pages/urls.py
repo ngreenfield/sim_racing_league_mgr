c8a64d72c67f6f538a023cae8e26a4809e445b22
Nick Greenfield <nagreenfield@yahoo.com>
2025-05-17 11:40:27 -0700

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from pages import views
+
+
+urlpatterns = [
+    path('', views.home_view, name="root"),
+    path('home/', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact')
+]
