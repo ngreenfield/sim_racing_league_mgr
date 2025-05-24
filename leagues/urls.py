100bff25c9c078e52e7290dd8b1819e0dd784daa
Nick Greenfield <nagreenfield@yahoo.com>
2025-05-24 11:27:50 -0700

    added full CRUD support and updated Home and About pages

--- leagues/urls.py
+++ leagues/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     path('list/', views.LeagueList.as_view(), name="league_list"),
     path('create/', views.LeagueCreate.as_view(), name="league_create"),
+    path('details/<int:pk>/', views.LeagueDetail.as_view(), name="league_details"),
+    path('update/<int:pk>/', views.LeagueUpdate.as_view(), name="league_update"),
+    path('delete/<int:pk>', views.LeagueDelete.as_view(), name="league_delete"),
 ]
--- leagues/views.py
+++ leagues/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import League
 from .forms import LeagueForm
 from django.urls import reverse_lazy
@@ -14,4 +14,32 @@ class LeagueCreate(CreateView):
     model = League
     form_class = LeagueForm
     template_name = "leagues/create.html"
+    success_url = reverse_lazy('league_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Create League"
+        return context
+
+
+class LeagueDetail(DetailView):
+    model = League
+    template_name = "leagues/details.html"
+
+
+class LeagueUpdate(UpdateView):
+    model = League
+    form_class = LeagueForm
+    template_name = "leagues/create.html"
+    success_url = reverse_lazy("league_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Update League"
+        return context
+    
+
+class LeagueDelete(DeleteView):
+    model = League
+    template_name = "leagues/delete.html"
     success_url = reverse_lazy('league_list')
