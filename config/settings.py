aa4ad4481dbd5a12442f1c4803c217ad7c047367
Nick Greenfield <nagreenfield@yahoo.com>
2025-06-17 18:31:10 -0700

    initial deployment

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-2!p&h2d^xg23s(qv&4+bsuz885idx!r^7dc*+wsnaqicl*+^*5
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'sim-league-hub-3a340a973d16.herokuapp.com/'
+    'sim-league-hub-3a340a973d16.herokuapp.com'
 ]
 
 
